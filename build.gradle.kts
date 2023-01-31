a0e1a09931fbaaa9fa6737e354d640f44b4c88ee
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2023-01-31 11:35:36 +0000

    Update build.gradle.kts (#113)

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ plugins {
 
 project.version = scmVersion.version
 
-val creekVersion = "0.3.1"
+val creekVersion = "0.3.2-SNAPSHOT"
 val spotBugsVersion = "4.7.3"         // https://mvnrepository.com/artifact/com.github.spotbugs/spotbugs-annotations
 val guavaVersion = "31.1-jre"         // https://mvnrepository.com/artifact/com.google.guava/guava
 val log4jVersion = "2.19.0"           // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
